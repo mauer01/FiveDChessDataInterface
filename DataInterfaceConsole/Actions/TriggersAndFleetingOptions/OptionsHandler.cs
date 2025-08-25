2f412ccfe0ae00b092b91b3b15ed9f255d3bfa16
mauer01 <17260044+mauer01@users.noreply.github.com>
2025-08-26 00:11:17 +0200

    add(ManageFleetingOptionsAndTriggers)

---
+++ DataInterfaceConsole/Actions/TriggersAndFleetingOptions/OptionsHandler.cs
@@ -0,0 +1,44 @@
+﻿using DataInterfaceConsole.Actions.Settings;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DataInterfaceConsole.Actions.TriggersAndFleetingOptions
+{
+    class OptionsHandler
+    {
+
+        public ISettingsValue[] GetOptions() => this.OptionsStore.Values.ToArray();
+
+        private readonly Dictionary<string, ISettingsValue> OptionsStore = new Dictionary<string, ISettingsValue>(); // string : OptionsValue<T>
+        private void AddOption(ISettingsValue s)
+        {
+            this.OptionsStore.Add(s.Id, s);
+        }
+
+        private ISettingsValue GetOption(string Id) => this.OptionsStore[Id];
+
+
+        public OptionsHandler()
+        {
+            AddOption(new SettingsValueWhitelisted<string>("UndoSubmittedMoves", "Undo Submitted Moves", "unlocks the ability to Undo submitted moves in Single/Local Games",
+                new[] { "on", "off" }, "off"));
+
+        }
+
+        public void WriteOptionIntoMemory(ISettingsValue changedOption)
+        {
+            var di = Program.instance.di;
+            switch (changedOption.Id)
+            {
+                case "UndoSubmittedMoves":
+                    di.MemLocUndoMoveReducedByValue.SetValue((byte)(changedOption.GetValueAsString() == "off" ? 0xFF : 0x00));
+                    break;
+            }
+        }
+        public static OptionsHandler LoadOrCreateNew()
+        {
+            var oh = new OptionsHandler();
+            return oh;
+        }
+    }
+}
